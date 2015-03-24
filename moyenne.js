6fc50ea8b03f3150213a10cd029908eea7331e18
epayet <jakspinning@gmail.com>
2015-03-24 13:27:41 +0100

    Gère les absences

--- moyenne.js
+++ moyenne.js
@@ -1,12 +1,16 @@
 var averageNotation = 0,
-	notes = $('.moy-note');
+	notes = $('.moy-note'),
+    nbAbsent = 0;
 
 notes.each(
 	function(index, element) {
-		averageNotation += parseFloat(element.innerHTML);
+        if(element.innerHTML != "Absent")
+		    averageNotation += parseFloat(element.innerHTML);
+        else
+            nbAbsent++;
 	}
 );
 
-averageNotation = (averageNotation / notes.length).toFixed(2);
+averageNotation = (averageNotation / (notes.length - nbAbsent)).toFixed(2);
 
 $('.page-title').append($('<h3>Moyenne générale estimée : ' + averageNotation + '</h3>'));
