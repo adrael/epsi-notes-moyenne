5646efe7405fa1050f6169c2c007f735e5ad6dc2
Raphaël MARQUES <work.rmarques@gmail.com>
2015-03-24 11:18:24 +0100

    First commit

---
+++ moyenne.js
@@ -0,0 +1,12 @@
+var averageNotation = 0,
+	notes = $('.moy-note');
+
+notes.each(
+	function(index, element) {
+		averageNotation += parseFloat(element.innerText);
+	}
+);
+
+averageNotation = (averageNotation / notes.length).toFixed(2);
+
+$('.page-title').append($('<h3>Moyenne générale estimée : ' + averageNotation + '</h3>'));
