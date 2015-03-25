a76f26345d95defba9b2168e03aae3075965a25d
damien <damiengabrielle@gmail.com>
2015-03-25 11:08:59 +0100

    Correction du précédent commit
    nbrNotes => nbNotes

--- moyenne.js
+++ moyenne.js
@@ -12,7 +12,7 @@ for (var i = 0, size = notes.length; i < size; ++i) {
 }
 
 var title = document.createElement('h3');
-title.innerHTML = (nbrNotes > 0) ? 'Moyenne générale estimée : ' + (sum / nbNotes).toFixed(2) : 'Aucune de note valide';
+title.innerHTML = (nbNotes > 0) ? 'Moyenne générale estimée : ' + (sum / nbNotes).toFixed(2) : 'Aucune de note valide';
 document.querySelector('.page-title').appendChild(title);
 
 // pour le style
